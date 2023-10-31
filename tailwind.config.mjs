0b4c40a3684689064012442749724f48ae686feb
Udeorah Frank <franklynudeorah@gmail.com>
2023-10-31 11:14:36 +0100

    create layout component, create navbar and add custom tailwind styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			maxWidth: {
+				custom: '1240px'
+			},
+			fontFamily: {
+				headerFont: ['Russo One']
+			}
+		},
 	},
 	plugins: [],
 }
