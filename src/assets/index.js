1d4676c39a0c3141139e00f59bf719781bfdb0af
Udeorah Frank <franklynudeorah@gmail.com>
2023-11-18 22:44:48 +0100

    import assets used in the site

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import vid from "./vid.jpg";
+import x from "./twitter.svg";
+import instagram from "./instagram.svg";
+import linkedIn from "./linkedin.svg";
+import mail from "./mail.svg";
+
+export {vid, x, instagram, linkedIn, mail};
